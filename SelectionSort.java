9936273c5290f10116282376432302a9b168e6e2
Richa Vinzey <richavinzey@gmail.com>
2022-09-20 14:04:42 +0530

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -7,19 +7,19 @@ import java.util.Arrays;
  * */
 public class SelectionSort {
   public static void main(String[] args) {
-    int temp = 999999999, index =-1;
+    int temp, index;
     int[] array = {10,32,43,21,89,24};
     System.out.println("Input Array is "+Arrays.toString(array));
     for(int i = 0;i<array.length-1;i++){
       index =i;
       for(int j = i+1; j<array.length;j++){
-        if (array[j] < array[index]) {
+        if (array[j] < array[i]) {
           index = j;
         }
-        temp = array[i];
-        array[i] = array[index];
-        array[index] = temp;
       }
+      temp = array[i];
+      array[i] = array[index];
+      array[index] = temp;
     }
     System.out.println("Output Array is "+Arrays.toString(array));
   }
