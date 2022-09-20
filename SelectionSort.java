b61352d93af9ef3eeefee2fe016b57de1389e0fb
Richa Vinzey <richavinzey@gmail.com>
2022-09-20 14:01:39 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,26 @@
+package sortingalogrithms;
+
+import java.util.Arrays;
+/*
+ * This class is to demonstrate the selection-sort algorithm
+ *
+ * */
+public class SelectionSort {
+  public static void main(String[] args) {
+    int temp = 999999999, index =-1;
+    int[] array = {10,32,43,21,89,24};
+    System.out.println("Input Array is "+Arrays.toString(array));
+    for(int i = 0;i<array.length-1;i++){
+      index =i;
+      for(int j = i+1; j<array.length;j++){
+        if (array[j] < array[index]) {
+          index = j;
+        }
+        temp = array[i];
+        array[i] = array[index];
+        array[index] = temp;
+      }
+    }
+    System.out.println("Output Array is "+Arrays.toString(array));
+  }
+}
